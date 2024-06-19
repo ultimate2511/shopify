198baeaedac478caa515a4e3235348418bf39b90
anish <anishraj.ar21@gmail.com>
2024-06-19 15:28:03 +0530

    routing has ben done

---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,12 @@
+//to render different categories like men women kids
+import React from 'react'
+
+const ShopCategory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShopCategory
