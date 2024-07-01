180f5082e8e8e5130a069967269fb09e6177d3ec
anish <anishraj.ar21@gmail.com>
2024-07-01 21:08:17 +0530

    login signup completed

--- server/index.js
+++ server/index.js
@@ -33,6 +33,16 @@ app.post('/upload',upload.single('product'),(req,res)=>{
        })
 });
 
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+    name: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    cartData: { type: Object },
+    date: { type: Date, default: Date.now() },
+  });
+  
+
 // schema for the products
 
 const Product=mongoose.model("Product",{
@@ -71,9 +81,99 @@ const Product=mongoose.model("Product",{
     },
 });
 
+
+// Create an endpoint at ip/login for login the user and giving auth-token
+app.post('/login', async (req, res) => {
+    console.log("Login");
+    let success = false;
+    let user = await Users.findOne({ email: req.body.email });
+    if (user) {
+      const passCompare = req.body.password === user.password;
+      if (passCompare) {
+        const data = {
+          user: {
+            id: user.id
+          }
+        }
+        success = true;
+        console.log(user.id);
+        const token = jwt.sign(data, 'secret_ecom');
+        res.json({ success, token });
+      }
+      else {
+        return res.status(400).json({ success: success, errors: "please try with correct email/password" })
+      }
+    }
+    else {
+      return res.status(400).json({ success: success, errors: "please try with correct email/password" })
+    }
+  })
+//Create an endpoint at ip/auth for regestring the user & sending auth-token
+app.post('/signup', async (req, res) => {
+    console.log("Sign Up");
+    let success = false;
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+      return res.status(400).json({ success: success, errors: "existing user found with this email" });
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+      cart[i] = 0;
+    }
+    const user = new Users({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      cartData: cart,
+    });
+    await user.save();
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+  
+    const token = jwt.sign(data, 'secret_ecom');
+    success = true;
+    res.json({ success, token })
+  });
+
+  
+// // Create an endpoint for saving the product in cart
+// app.post('/addtocart', fetchuser, async (req, res) => {
+//     console.log("Add Cart");
+//     let userData = await Users.findOne({ _id: req.user.id });
+//     userData.cartData[req.body.itemId] += 1;
+//     await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+//     res.send("Added")
+//   })
+  
+  
+//   // Create an endpoint for removing the product in cart
+//   app.post('/removefromcart', fetchuser, async (req, res) => {
+//     console.log("Remove Cart");
+//     let userData = await Users.findOne({ _id: req.user.id });
+//     if (userData.cartData[req.body.itemId] != 0) {
+//       userData.cartData[req.body.itemId] -= 1;
+//     }
+//     await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+//     res.send("Removed");
+//   })
+  
+  
+//   // Create an endpoint for getting cartdata of user
+//   app.post('/getcart', fetchuser, async (req, res) => {
+//     console.log("Get Cart");
+//     let userData = await Users.findOne({ _id: req.user.id });
+//     res.json(userData.cartData);
+  
+//   })
+  
+    
+
 app.post('/addProduct',async(req,res)=>{
        
-    let products=await Product.find(({}));
+    let products=await Product.find({});
     let id=1;
     if(products.length>0){
 
