9353585e60d53e44d56e0f5491bf267881d6a4fa
anish <anishraj.ar21@gmail.com>
2024-07-02 19:42:29 +0530

    click on div to open product page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom'
 
 const Item = (props) => {
   return (
-    <div className='item'>
-       
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)}src={props.image} alt="" /></Link> 
-       <p>{props.name}</p>
-       <div className="item-prices">
-        <div className="item-price-new">
+    <div className='item' onClick={() => window.scrollTo(0, 0)}>
+      <Link to={`/product/${props.id}`} style={{ textDecoration: 'none' }}>
+        <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt="" />
+        <p style={{color:'black'}} onClick={() => window.scrollTo(0, 0)}>{props.name}</p>
+        <div className="item-prices">
+          <div className="item-price-new">
             ${props.new_price}
-        </div>
-        <div className="item-price-old">
+          </div>
+          <div className="item-price-old">
             ${props.old_price}
+          </div>
         </div>
-       </div>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Item
