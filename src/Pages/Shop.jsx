bfeb0578e490e852525d5a04f46e0f86d017be40
anish <anishraj.ar21@gmail.com>
2024-07-02 19:13:46 +0530

    popular in women feature is added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
-
 const Shop = () => {
+
+  const [popular, setPopular] = useState([]);
+  
+
+  const fetchInfo = () => { 
+    fetch('https://shopify-backend-gezh.onrender.com/popularinwomen') 
+            .then((res) => res.json()) 
+            .then((data) => setPopular(data));
+   
+    }
+
+    useEffect(() => {
+      fetchInfo();
+    }, [])
+
+
   return (
     <div>
       <Hero/>
-      <Popular/>
+      <Popular data={popular}/>
       <Offers/>
-      <NewCollections/>
+      <NewCollections />
       <NewsLetter/>
-     
-  
     </div>
   )
 }
